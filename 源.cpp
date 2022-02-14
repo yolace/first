6b50dbe3495bd45744d11f19a511e7c644255d28
yolace <98695026+yolace@users.noreply.github.com>
2022-02-14 16:10:12 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,28 +1,104 @@
-#include < iostream>
+#include<iostream>
 #include<vector>
 using namespace std;
+
+
 int main()
-{    
-	int n;
-	cout << "请键入向量的元素量：";
-	cin >> n;
-	 vector<float> a(n, 0);       //对其中部分元素进行输入
-	 vector<float> b(n, 0);
-	int i;
-	cout << "请键入向量1的元素"<<endl;
-	for (i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	cout << "请键入向量2的元素"<<endl;
-	float dot_product = 0; //设定向量点积
-	for (i = 0; i < n; i++)
-	{
-		cin >> b[i];
-		dot_product += a[i] * b[i];
-	}
-	
-	cout << dot_product;
-	
-      return 0;
+{
+     
+    int n1, n2, n3, n4, i, j, m,l;
+    cout << "输入矩阵1的行列："<<endl; 
+    cin >> n1 >> n2;
+   
+   
+    float a[100][100];
+    float b[100][100];
+    float c[100][100];
+    cout << "矩阵1："<<endl;
+    for (int i = 0; i < n1; i++)
+        for (int j = 0; j < n2; j++)
+        {
+            cin >> a[i][j];
+            a[i][j + 1] = 0;
+            a[i+1][j] = 0;
+        }
+    cout << "输入矩阵2的行列："<<endl;
+    cin >> n3 >> n4;
+    cout << "矩阵2：" <<endl;
+    for (int i = 0; i < n3; i++)
+        for (int j = 0; j < n4; j++)
+        {
+            cin >> b[i][j];
+            b[i][j + 1] = 0;
+            b[i + 1][j] = 0;
+        }
+    float sum = 0;
+    cout << "运算符：";
+     char t;
+     cin >> t;
+     switch (t)
+     {
+     case '+':
+         if (n1 != n3 || n2 != n4)
+         {
+             cout << "不符合运算规则！";
+             return 0;
+         }
+         for (i = 0; i < n1; ++i)
+         {
+             for (j = 0,m = 0; j < n2; ++j)
+             {
+                 sum += a[i][j] + b[i][j];
+                 cout << sum << " ";
+                 sum = 0;
+             }
+             cout << endl;
+         }
+         break;
+     case'-':
+          if (n1 != n3 || n2 != n4)
+         {
+             cout << "不符合运算规则！";
+             return 0;
+         }
+         for (i = 0; i < n1; ++i)
+         {
+             for (j = 0; j < n2; ++j)
+             {
+                 sum += a[i][j] - b[i][j];
+                 cout << sum << " ";
+                 sum = 0;
+             }
+             cout << endl;
+         }
+         break;
+     case'*':
+         if (n2 != n3) {
+             cout << "不符合运算规则！";
+             return 0;
+
+         }
+         for (i = 0; i < n1; i++)
+         {
+             for (j = 0, m = 0; j < n2; j++, m++)
+             {
+           
+                 for (l = j; l < n2; l++)
+                 {
+                     sum += a[i][l] + b[l][i + m];
+                 }
+                 cout << sum << " ";
+                 sum = 0;
+          
+             }
+             cout << endl;
+
+         }
+      
+         break;
+
+
+     };
+   
+ 
 }
